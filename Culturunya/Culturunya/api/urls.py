be705bd7fea98d6f73a597d581344d46bfd8acb1
Richard Pie Sánchez <140947112+richardpie@users.noreply.github.com>
2025-03-08 19:06:09 +0100

    Back - API working

---
+++ Culturunya/Culturunya/api/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from .views import test_api
+from .views import data_test
+urlpatterns = [#se concatena con el path de url del proyecto
+    path('test/', test_api, name='test_api'),
+    path('data/', data_test, name='data_test')
+]
---
+++ Culturunya/Culturunya/api/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.http import JsonResponse
+
+def test_api(request):
+    return JsonResponse({"message": "Testing API okay"})
+def data_test(request):
+    return JsonResponse({"message": "Testing API DATA"})
