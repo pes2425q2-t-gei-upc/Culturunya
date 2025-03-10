1d8eee65bfd895a75f22bfb5cbbf92cb818e58ed
Richard Pie Sánchez <140947112+richardpie@users.noreply.github.com>
2025-03-10 18:34:09 +0100

    Back - Delete api call

--- Culturunya/Culturunya/api/urls.py
+++ Culturunya/Culturunya/api/urls.py
@@ -2,7 +2,9 @@
 from django.urls import path
 from .views import test_api
 from .views import data_test
+from .views import delete_test
 urlpatterns = [#se concatena con el path de url del proyecto
     path('test/', test_api, name='test_api'),
-    path('data/', data_test, name='data_test')
+    path('data/', data_test, name='data_test'),
+    path('delete/', delete_test, name='delete_test'),
 ]
--- Culturunya/Culturunya/api/views.py
+++ Culturunya/Culturunya/api/views.py
@@ -3,5 +3,12 @@ from django.http import JsonResponse
 
 def test_api(request):
     return JsonResponse({"message": "Testing API okay"})
+
 def data_test(request):
     return JsonResponse({"message": "Testing API DATA"})
+
+def delete_test(request):
+    if request.method == "DELETE":
+        return JsonResponse({"message": "Delete request received"})
+    else:
+        return JsonResponse({"error": "Invalid request method"}, status=400)
