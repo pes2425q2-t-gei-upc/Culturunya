7896bcb5eb80b28d63e09289351ed58942fd1628
Richard Pie Sánchez <140947112+richardpie@users.noreply.github.com>
2025-03-10 17:38:46 +0100

    Back - first migration

--- Culturunya/Culturunya/persistence/models.py
+++ Culturunya/Culturunya/persistence/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    comarca = models.CharField(max_length=100)
+    province = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.address}, {self.city}, {self.province}"
