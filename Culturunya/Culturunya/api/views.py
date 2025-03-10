a1d9cfc33044d2ad30b5407668e3d953f0f95dd7
Richard Pie Sánchez <140947112+richardpie@users.noreply.github.com>
2025-03-10 18:46:23 +0100

    Back - csrf_exempt provisional

--- Culturunya/Culturunya/api/views.py
+++ Culturunya/Culturunya/api/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def test_api(request):
     return JsonResponse({"message": "Testing API okay"})
-
+@csrf_exempt
 def data_test(request):
     return JsonResponse({"message": "Testing API DATA"})
-
+@csrf_exempt
 def delete_test(request):
     if request.method == "DELETE":
         return JsonResponse({"message": "Delete request received"})
