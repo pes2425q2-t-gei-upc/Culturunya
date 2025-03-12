a3b0c704ed502ad2d5c0b0fcf0d2ea97a2224160
Martin Viteri <m.viteribalcazar@gmail.com>
2025-03-12 18:07:49 +0100

    Tasca#67: Crear model event i category fet

--- Culturunya/Culturunya/persistence/models.py
+++ Culturunya/Culturunya/persistence/models.py
@@ -11,3 +11,34 @@ class Location(models.Model):
 
     def __str__(self):
         return f"{self.address}, {self.city}, {self.province}"
+
+class Category(models.Model):
+    TYPE_CATEGORY_CHOICES = [
+        ('Comedy', 'Comedy'),
+        ('Theater', 'Theater'),
+        ('Cinema', 'Cinema'),
+        ('ForChildren', 'ForChildren'),
+        ('Exposition', 'Exposition'),
+        ('Show', 'Show'),
+        ('Carnival', 'Carnival'),
+        ('Concert', 'Concert'),
+        ('HolidayEvent', 'Holiday Event'),
+        ('Festival', 'Festival'),
+        ('Dance', 'Dance'),
+        ('Conference', 'Conference'),
+    ]
+    category = models.CharField(max_length=255, choices=TYPE_CATEGORY_CHOICES, unique=True)
+    def __str__(self):
+        return f"{self.category}"
+
+class Event(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    name = models.CharField(max_length=255)
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField()
+    description = models.TextField()
+    price = models.FloatField()
+    categories = models.ManyToManyField(Category, related_name='events')
+
+    def __str__(self):
+        return f"{self.name}"
