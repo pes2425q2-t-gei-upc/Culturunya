7c4344628ade883867ce074e7feadcc4b8fb9ec3
Richard Pie Sánchez <140947112+richardpie@users.noreply.github.com>
2025-03-17 08:26:53 +0100

    Back - Tenemos datos en la bd

--- Culturunya/Culturunya/persistence/models.py
+++ Culturunya/Culturunya/persistence/models.py
@@ -9,27 +9,25 @@ class Location(models.Model):
     comarca = models.CharField(max_length=100)
     province = models.CharField(max_length=100)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['longitude', 'latitude'], name='unique_lat_long')
+        ]
+
     def __str__(self):
         return f"{self.address}, {self.city}, {self.province}"
 
+
 class Category(models.Model):
-    TYPE_CATEGORY_CHOICES = [
-        ('Comedy', 'Comedy'),
-        ('Theater', 'Theater'),
-        ('Cinema', 'Cinema'),
-        ('ForChildren', 'ForChildren'),
-        ('Exposition', 'Exposition'),
-        ('Show', 'Show'),
-        ('Carnival', 'Carnival'),
-        ('Concert', 'Concert'),
-        ('HolidayEvent', 'Holiday Event'),
-        ('Festival', 'Festival'),
-        ('Dance', 'Dance'),
-        ('Conference', 'Conference'),
-    ]
-    category = models.CharField(max_length=255, choices=TYPE_CATEGORY_CHOICES, unique=True)
+    name = models.CharField(max_length=255, unique=True)
+
     def __str__(self):
-        return f"{self.category}"
+        return self.name
+
+    @staticmethod
+    def get_or_create_category(category_name):
+        category, created = Category.objects.get_or_create(name=category_name)
+        return category
 
 class Event(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
@@ -37,7 +35,8 @@ class Event(models.Model):
     date_start = models.DateTimeField()
     date_end = models.DateTimeField()
     description = models.TextField()
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='events', null=True)
     categories = models.ManyToManyField(Category, related_name='events')
 
     def __str__(self):
