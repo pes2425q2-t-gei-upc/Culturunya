557ca38753ad2c9a0d76613aac1e3e008a925b7e
DESKTOP-FUGT6TB\R <richard.pie@estudiantat.upc.edu>
2025-03-21 19:40:50 +0100

    Back - Swagger API

--- Culturunya/Culturunya/Culturunya/settings.py
+++ Culturunya/Culturunya/Culturunya/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'persistence',
+    'drf_yasg',
 ]
 
 # Middleware framework
--- Culturunya/Culturunya/Culturunya/urls.py
+++ Culturunya/Culturunya/Culturunya/urls.py
@@ -19,10 +19,28 @@ Including another URLconf
 # Uncomment next two lines to enable admin:
 #from django.contrib import admin
 #from django.urls import path
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view( #http://127.0.0.1:8000/swagger/
+   openapi.Info(
+      title="API",
+      default_version='v1',
+      description="Swagger",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     # Uncomment the next line to enable the admin:
     #path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', 
+            schema_view.without_ui(cache_timeout=0), 
+            name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
