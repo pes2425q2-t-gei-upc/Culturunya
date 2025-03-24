0d14de82b60671afa7b2fdb637dcaefd95e971e8
Martin Viteri <m.viteribalcazar@gmail.com>
2025-03-24 18:39:04 +0100

    Modelos de todas las clases menos el calendario

--- Culturunya/Culturunya/persistence/models.py
+++ Culturunya/Culturunya/persistence/models.py
@@ -101,6 +101,17 @@ class User(AbstractUser):
     points_to_next_quiz_points = models.IntegerField(default=POINTS_TO_NEXT_RANK[TypeRank.UNRANKED])
     banned_from_comments = models.BooleanField(default=False)
 
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="custom_user_groups",
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="custom_user_permissions",
+        blank=True
+    )
+
     def __str__(self):
         return self.username
 
