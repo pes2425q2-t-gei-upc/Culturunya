58b7cc07cdb082824e30f257fbf81344d54de2d6
mapsdos <arnauprohens@gmail.com>
2025-04-01 16:21:37 +0200

    Error AppNavigation arreglat

--- app/src/main/java/com/example/culturunya/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/culturunya/navigation/AppNavigation.kt
@@ -6,7 +6,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.culturunya.endpoints.events.EventViewModel
-import com.example.culturunya.screens.ComposableCanviContrasenya
 import com.example.culturunya.screens.ComposableIniciSessio
 import com.example.culturunya.screens.MainScreen
 import com.example.culturunya.screens.PantallaRegistre
