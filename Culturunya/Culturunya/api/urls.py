efaadf00e8917cfb9a89c8632970d3170f594cd9
Martin Viteri <m.viteribalcazar@gmail.com>
2025-04-20 13:52:32 +0200

    Task #142: cambio de nombre de usuario done + cambio de url de cambio de contraseña

--- Culturunya/Culturunya/api/urls.py
+++ Culturunya/Culturunya/api/urls.py
@@ -5,6 +5,7 @@ from .views import (
     get_events, get_filtered_events, create_user, create_rating_endpoint,
     CustomObtainAuthToken, delete_own_account, ChangePasswordView, UserProfileView, get_conversation_with_admin,
     get_conversation_with_user, send_message_user_to_admin, send_message_admin_to_user, update_language,
+    update_username,
 )
 from .views import data_test
 from .views import delete_test
@@ -19,17 +20,16 @@ urlpatterns = [#se concatena con el path de url del proyecto
     path('events/filter/', get_filtered_events, name='get_filtered_events'),
     #un endpoint GET para obtener los eventos que un usuario tiene en su calendario personal
     #otro endpoint POST/DELETE para añadir/quitar eventos de su calendario personal
-
     path('create_user/', create_user, name='create_user'),
     path('ratings/', create_rating_endpoint, name='create_rating'),
     path('login/', CustomObtainAuthToken.as_view(), name='api_token_auth'),
     path('delete_account/', delete_own_account, name='delete_own_account'),
-    path('change_password/', ChangePasswordView.as_view(), name='change_password'),
-    path('user/profile/', UserProfileView.as_view(), name='user_profile'),
     path('chat/send_to_admin/', send_message_user_to_admin, name='send_to_admin'),
     path('chat/send_to_user/', send_message_admin_to_user, name='send_to_user'),
     path('chat/with_admin/', get_conversation_with_admin, name='get_conversation_with_admin'),
     path('chat/with_user/<int:user_id>', get_conversation_with_user, name='get_conversation_with_user'),
+    path('user/profile_info/', UserProfileView.as_view(), name='user_profile'),
+    path('user/change_password/', ChangePasswordView.as_view(), name='change_password'),
     path('user/update_language/', update_language, name='update_language'),
-
+    path('user/update_username/', update_username, name='update_username'),
 ]
