2899e85e67c8525d5566b32fe13c37b065a2114b
Martin Viteri <m.viteribalcazar@gmail.com>
2025-04-22 20:43:11 +0200

    Cambio de post a put en el endpoint de cambio de contraseña

--- Culturunya/Culturunya/api/views.py
+++ Culturunya/Culturunya/api/views.py
@@ -394,6 +394,7 @@ class ChangePasswordView(APIView):
     permission_classes = [IsAuthenticated]
 
     @swagger_auto_schema(
+        method="put",
         request_body=ChangePasswordSerializer,
         operation_summary="Cambiar contraseña",
         operation_description="Permite a un usuario autenticado cambiar su contraseña actual.",
@@ -403,7 +404,7 @@ class ChangePasswordView(APIView):
             401: "No autenticado.",
         }
     )
-    def post(self, request):
+    def put(self, request):
         serializer = ChangePasswordSerializer(data=request.data)
         if serializer.is_valid():
             user = request.user
