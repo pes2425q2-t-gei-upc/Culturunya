b1391a2499a056c07cfafa1d0e84e3e276f90c9f
kirian.roca <kirian.roca@estudiantat.upc.edu>
2025-04-28 02:52:29 +0200

    Formulario para añadir comentario (sin Rating) y botón para comentar

--- app/src/main/java/com/example/culturunya/screens/RatingList.kt
+++ app/src/main/java/com/example/culturunya/screens/RatingList.kt
@@ -3,22 +3,44 @@ package com.example.culturunya.screens
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.culturunya.R
 import com.example.culturunya.endpoints.events.RatingViewModel
 import com.example.culturunya.endpoints.events.Rating
+import com.example.culturunya.models.currentSession.CurrentSession
 import com.example.culturunya.ui.theme.*
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun RatingListScreen(
@@ -32,23 +54,72 @@ fun RatingListScreen(
     ratingViewModel.fetchRatingsForEvent(eventId)
     // Collect the ratings
     val ratings by ratingViewModel.ratings.collectAsState()
+    var rating_new by remember { mutableStateOf("") }
+    val context = LocalContext.current
+    CurrentSession.getInstance()
+    val currentLocale by remember { mutableStateOf(CurrentSession.language) }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        // Display error if needed
-        error?.let { errorMessage ->
-            Text(text = errorMessage, color = Color.Red)
-        }
-        if (ratings.isEmpty() && error == null) {
-            Text("No ratings yet", color = Purple40)
-        } else {
-            Column(
+        Column(
                 modifier = Modifier
                     .background(Color.White)
-            ) {
+        ) {
+            Box(modifier = Modifier.fillMaxWidth()) {
+                Column(modifier= Modifier.fillMaxWidth()) {
+                    OutlinedTextField(
+                        value = rating_new,
+                        onValueChange = { rating_new = it },
+                        label = { Text(getString(context, R.string.Comment, currentLocale)) },
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(5.dp),
+                        leadingIcon = {
+                            Icon(imageVector = Icons.Default.Person, contentDescription = "Persona")
+                        },
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            textColor = Color.Black,
+                            cursorColor = Color.Black,
+                            focusedBorderColor = Color.Gray,
+                            unfocusedBorderColor = Color.LightGray
+                        )
+                    )
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Button(
+                            modifier = Modifier.width(200.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Purple40,
+                                contentColor = Color.White
+                            ),
+                            onClick = {
+                                // logica Comentar
+                            }
+                        ) {
+                            Text(
+                                text = "Comentar",
+                                modifier = Modifier.padding(2.dp),
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
+            }
+            Divider(modifier = Modifier.padding(vertical = 8.dp))
+                // Display error if needed
+            error?.let { errorMessage ->
+                Text(text = errorMessage, Modifier.padding(4.dp), color = Color.Red)
+            }
+            if (ratings.isEmpty() && error == null) {
+                    Text("No ratings yet", color = Purple40)
+            } else {
                 ratings.forEach { rating ->
                     RatingBox(rating = rating, onRatingClick = { onRatingSelected(rating) })
                 }
