859dbedf12250dae4cb2141fbcde0cda9d1800d8
kirian.roca <kirian.roca@estudiantat.upc.edu>
2025-05-21 21:08:50 +0200

    Tasca #177 Nova icona per reportar una valoració i canvi de botó a icona

--- app/src/main/java/com/example/culturunya/views/RatingBox.kt
+++ app/src/main/java/com/example/culturunya/views/RatingBox.kt
@@ -5,24 +5,35 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.culturunya.R
 import com.example.culturunya.dataclasses.ratings.Rating
 import com.example.culturunya.CurrentSession
 import com.example.culturunya.ui.theme.*
+import com.google.android.gms.maps.model.Circle
 
 @Composable
 fun RatingBox(
@@ -47,20 +58,27 @@ fun RatingBox(
                         fontSize = 12.sp,
                         color = Purple40
                     ), modifier = Modifier.padding(top = 8.dp, bottom = 4.dp))
-                Spacer(modifier = Modifier.padding(30.dp))
-                var bsize by remember { mutableStateOf(100.dp) }
-                if(currentLocale == "en") bsize = 85.dp
-                Button(
-                     modifier = Modifier.width(bsize),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Red,
+                Spacer(Modifier.weight(1f))
+                IconButton(
+                    modifier = Modifier
+                        .size(35.dp)
+                        .background(Color.Red, CircleShape)
+                        .clip(CircleShape)
+                    ,
+                    colors = IconButtonDefaults.iconButtonColors(
+                        containerColor = Color.Transparent,
                         contentColor = Color.White
                     ),
                     onClick = {
                         // logica reportar
                     }
                 ){
-                    Text(text = getString(context, R.string.Report, currentLocale), modifier = Modifier.background(Color.Red), color= Color.White, fontSize = 12.sp)
+                    Icon(
+                            painter = painterResource(id = R.drawable.ic_report),
+                            contentDescription = stringResource(id = R.string.Report),
+                            tint = Color.White,
+                            modifier = Modifier.size(25.dp)
+                    )
                 }
             }
             //Spacer(modifier = Modifier.padding(8.dp))
