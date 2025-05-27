59c69378b7bc958bec58d0d28b7fbd138fd3aa59
Richard Pie Sánchez <140947112+richardpie@users.noreply.github.com>
2025-05-27 13:03:48 +0200

    id user to use in admin actions

--- Culturunya/Culturunya/api/serializers.py
+++ Culturunya/Culturunya/api/serializers.py
@@ -8,6 +8,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
+            'id',
             'username',
             'first_name',
             'last_name',
